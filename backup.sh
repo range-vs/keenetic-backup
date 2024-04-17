4cc645f3a59745d84dd15ae370de7fab5924e1aa
range-vs <trubnicov_ivan1996@mail.ru>
2024-04-18 02:17:53 +0300

    + add logging & inspect

--- backup.sh
+++ backup.sh
@@ -1,6 +1,27 @@
 #!/bin/sh
 
-source .env
+if [ -f .env ]; then
+    source .env
 
-config=$(ssh $USER@$HOST 'show running-config')
-echo "$config"
+    if [ -z "$HOST" ]; then
+        echo "HOST not find in .env"
+        exit 1
+    fi
+
+    if [ -z "$USER" ]; then
+        echo "USER not find in .env"
+        exit 1
+    fi
+
+    if [ -z "$PASS" ]; then
+        echo "PASS not find in .env"
+        exit 1
+    fi
+
+    # Выполняем необходимые действия
+    config=$(ssh $USER@$HOST 'show running-config')
+    echo "$config"
+else
+    echo "File .env not found"
+    exit 1
+fi
